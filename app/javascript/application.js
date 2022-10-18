479bfe6ae3f1e1919529e621873f273075a62423
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-18 18:22:26 +0100

    Create async thunk to handle greeting fetch.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,26 +2,28 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Greeting from "./components/Greeting";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<div>Static page</div>}/>
-        <Route path="/greetings" element={<Greeting />}/>
+      <Routes>       
+        <Route path="/" element={<Greeting />}/>
       </Routes>
     </div>
   );
 }
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
+ReactDOM.render(  
+    <BrowserRouter>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </BrowserRouter>,
   document.getElementById('root'),
 );
--- app/javascript/components/greeting.js
+++ app/javascript/components/greeting.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/greetingSlice';
 
 const Greeting = () => {
+  const greeting = useSelector(state => state.greeting);
+  const dispatch = useDispatch();
+  console.log(greeting)
+  
   return (
-    <div>greeting</div>
+    <div>
+      <h1>{greeting.message}</h1>
+      <button
+        style={{ fontSize: '2em', padding: 20, backgroundColor: 'blue', color: 'white' }}
+        type="button"
+        onClick={() => {
+          dispatch(fetchGreeting())
+        }}
+      >
+        greet
+      </button>
+    </div>
   )
 }
 

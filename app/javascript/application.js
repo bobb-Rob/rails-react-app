7e506f2180235e1fd6c9f402f1b53106a5bed74d
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-18 16:09:12 +0100

    Add route to client side using react-router-dom

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,12 +5,23 @@ import "./controllers"
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greeting from "./components/Greeting";
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<div>Static page</div>}/>
+        <Route path="/greetings" element={<Greeting />}/>
+      </Routes>
+    </div>
+  );
 }
 
 ReactDOM.render(
-  <App/>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
   document.getElementById('root'),
 );

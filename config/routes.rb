8e0998f24d22de61e125658caeb951af8bf207bb
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-18 16:07:49 +0100

    Greeting controller return json as api vie /greeting route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
-  get 'greetings/index'
+Rails.application.routes.draw do 
   root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  namespace :api do
+    resources :greetings, only: [:index]
+  end
 end
